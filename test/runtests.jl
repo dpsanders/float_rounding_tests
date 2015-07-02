6f758af9ad3f81c77f4d9f055c1c718ec81f218b
David P. Sanders <dpsanders@gmail.com>
2015-07-02 12:51:06 -0400

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
+using Compat
+
 a = with_rounding(Float64, RoundDown) do
     1/10
 end
@@ -9,5 +11,21 @@ b = with_rounding(Float64, RoundUp) do
 end
 
 @show b
+
+
+@compat y = with_rounding(Float64, RoundDown) do
+    parse(Float64, "3.1")
+end
+
+@compat z = with_rounding(Float64, RoundDown) do
+    parse(Float64, "3.1")
+end
+
+@show y, z
+
+using ValidatedNumerics
+
+a = @interval(0.1, 0.3)
+@show a
 
 
